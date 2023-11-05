09d7efafb5d3cf1bf817f2c7368873b9c430266f
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2023-11-05 16:23:56 +0100

    storage: Link to related entities on admin pages

--- monosmdom_server/storage/admin.py
+++ monosmdom_server/storage/admin.py
@@ -1,10 +1,29 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 from storage import models
 
 # TODO: Search?
 # TODO: Filter?
 # TODO: Order?
 # TODO: Optimize using list_select_related?
+
+
+# TODO: This should live somewhere else.
+def item_type_to_name(item_type_char):
+    if item_type_char == "n":
+        return "node"
+    elif item_type_char == "w":
+        return "way"
+    elif item_type_char == "r":
+        return "relation"
+    else:
+        return f"ERROR{item_type_char}"
+
+
+# TODO: This should live somewhere else.
+def occ_to_editor_url(occ_obj):
+    return f"https://www.openstreetmap.org/edit?{item_type_to_name(occ_obj.osm_item_type)}={occ_obj.osm_item_id}"
 
 
 class ReadOnlyModelAdmin(admin.ModelAdmin):
@@ -21,6 +40,19 @@ class ReadOnlyModelAdmin(admin.ModelAdmin):
         return False
 
 
+class CrawlableUrlSelfInline(admin.TabularInline):
+    model = models.CrawlableUrl
+    readonly_fields = ["content_url", "crurl"]
+    fields = ["content_url", "crurl"]
+
+    @admin.display(description="URL")
+    def content_url(self, obj):
+        return obj.url.url
+
+    def crurl(self, obj):
+        return format_html("<a href={}>{}</a>", reverse("admin:storage_crawlableurl_change", args=(obj.id,)), str(obj))
+
+
 @admin.register(models.Domain)
 class DomainAdminForm(ReadOnlyModelAdmin):
     empty_value_display = "(never)"
@@ -29,11 +61,45 @@ class DomainAdminForm(ReadOnlyModelAdmin):
     # TODO: Action: Set last_contacted to "now"
     # TODO: Action: Clear last_contacted
 
+    inlines = [
+        CrawlableUrlSelfInline,
+    ]
+
+
+class CrawlableUrlDomainInline(admin.TabularInline):
+    model = models.CrawlableUrl
+    readonly_fields = ["domain"]
+    fields = ["domain"]
+
+
+class OsmOccurrenceInline(admin.TabularInline):
+    model = models.OccurrenceInOsm
+    readonly_fields = ["editor_url", "occ"]
+    fields = ["editor_url", "occ"]
+
+    def occ(self, obj):
+        return format_html("<a href={}>{}</a>", reverse("admin:storage_occurrenceinosm_change", args=(obj.id,)), str(obj))
+
+    def editor_url(self, obj):
+        osm_url = occ_to_editor_url(obj)
+        return format_html("<a href={0}>{0}</a>", osm_url)
+
+
+class DisasterUrlInline(admin.TabularInline):
+    model = models.DisasterUrl
+    readonly_fields = ["reason"]
+    fields = ["reason"]
+
 
 @admin.register(models.Url)
 class UrlAdminForm(ReadOnlyModelAdmin):
     list_display = ["url"]
     readonly_fields = ["url"]
+    inlines = [
+        CrawlableUrlDomainInline,
+        OsmOccurrenceInline,
+        DisasterUrlInline,
+    ]
     # TODO: Link to related objects (DisasterUrl, CrawlableUrl, query for CrawlResult, query for OccurrenceInOsm)
     # TODO: Link to OSM website
 
@@ -67,7 +133,7 @@ class CrawlableUrlAdminForm(ReadOnlyModelAdmin):
 @admin.register(models.OccurrenceInOsm)
 class OccurrenceInOsmAdminForm(ReadOnlyModelAdmin):
     list_display = ["truncated_url", "osm_entry", "tag"]
-    readonly_fields = ["url", "osm_item_type", "osm_item_id", "osm_tag_key", "osm_tag_value"]
+    readonly_fields = ["url", "osm_item_type", "osm_item_id", "osm_tag_key", "osm_tag_value", "edit_on_osm"]
     # TODO: Link to OSM website
 
     def truncated_url(self, obj):
@@ -81,6 +147,10 @@ class OccurrenceInOsmAdminForm(ReadOnlyModelAdmin):
         if len(tag_value) > 20:
             tag_value = tag_value[:20 - 1] + "…"
         return f"{obj.osm_tag_key}={tag_value}"
+
+    def edit_on_osm(self, obj):
+        osm_url = occ_to_editor_url(obj)
+        return format_html("<a href={0}>{0}</a>", osm_url)
 
 
 @admin.register(models.Import)
